b924652d4965425bf447ed5f4ed2d5ae547f2a42
Batoolayyad <batoolayyad1996@gmail.com>
2021-07-31 20:03:34 +0300

    auth

--- src/components/display.js
+++ src/components/display.js
@@ -1,57 +1,91 @@
-import axios from 'axios';
-import React from 'react';
-import { Form, Button } from 'react-bootstrap'
-class User extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      chefs: [],
-      showchefs: false
-    };
-  }
+// import axios from 'axios';
+// import React from 'react';
+// import { Form, Button } from 'react-bootstrap'
+// class User extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       chefs: [],
+//       showchefs: false
+//     };
+//   }
 
 
 
 
 
-  handleSubmit = async (e) => {
+//   handleSubmit = async (e) => {
 
-    e.preventDefault();
+//     e.preventDefault();
 
-    axios.post('http://localhost:3000/signin', {
-      username:this.state.username ,
-      pass: this.state.pass
-    })
-  }
+//     axios.post('http://localhost:3000/signin', {
+//       username:this.state.username ,
+//       pass: this.state.pass
+//     })
+//   }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-
+//   handleChange = (e) => {
+//     this.setState({ [e.target.name]: e.target.value });
+//   };
 
 
 
 
-  render() {
-    return (
-      <div >
+
+
+//   render() {
+//     return (
+//       <div >
         
-  {
-      this.state.showthem &&
-      this.state.result.map((item)=>{
-          return(
+//   {
+//       this.state.showthem &&
+//       this.state.result.map((item)=>{
+//           return(
 
-          )
-      })
-  }
+//           )
+//       })
+//   }
 
         
 
-      </div>
+//       </div>
 
-    );
-  }
-}
+//     );
+//   }
+// }
 
-export default User;
+// export default User;
+
+
+
+// import React from 'react'
+
+
+
+//  class display extends React.Component {
+//    constructor(props){
+//      super(props);
+//      this.state = {
+//       username: '',
+//       password: '',
+//       data: [],
+//       user: {},
+//       showlog:true
+//     };
+
+
+//   }
+ 
+    
+//   } 
+
+//   render() {
+//     return (
+//       <div>
+        
+//       </div>
+//     )
+//   }
+// }
+
+// export default display
