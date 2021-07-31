2ff8350af7fe377c8eceec653b0dafcfe8dc1d5a
bahaamq <bahaa.mq@gmail.com>
2021-07-31 18:29:25 +0300

    logininitial

---
+++ src/components/User.js
@@ -0,0 +1,81 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+import React, { Component } from 'react';
+
+class Login extends Component {
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:'',
+  data:[],
+  user:{},
+  };
+}
+  // eslint-disable-next-line no-undef
+  handleSubmit = async (e) => {
+
+ console.log("test")
+
+ var apiBaseUrl = "http://localhost:3000/";
+ var self = this;
+ var payload={
+ username:this.state.username,
+ password:this.state.password
+ }
+ const res= await axios.post(apiBaseUrl+'signin', payload,
+  {auth:
+     payload})
+
+  this.setState({
+    user:res.data,
+  
+  })
+
+console.log(res.data)
+
+// 2 components 
+// we have the user we have the role 
+// role : user // component : role:chef
+
+
+  }
+
+render() {
+    return (
+      <div>
+       
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+                          type="text"
+
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue.toString()})}
+             />
+           <br/>
+             <TextField
+               type="text"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue.toString()})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={this.handleSubmit}>
+               </RaisedButton>     </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  
+}
+const style = {
+ margin: 15,
+};
+export default Login;
---
+++ src/components/display.js
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import React from 'react';
+import { Form, Button } from 'react-bootstrap'
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chefs: [],
+      showchefs: false
+    };
+  }
+
+
+
+
+
+  handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    axios.post('http://localhost:3000/signin', {
+      username:this.state.username ,
+      pass: this.state.pass
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+
+
+
+
+
+  render() {
+    return (
+      <div >
+        
+  {
+      this.state.showthem &&
+      this.state.result.map((item)=>{
+          return(
+
+          )
+      })
+  }
+
+        
+
+      </div>
+
+    );
+  }
+}
+
+export default User;
