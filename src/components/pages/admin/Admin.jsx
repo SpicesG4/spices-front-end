65a869fa1804c0f7b3a28f3c881a6f2ed9f35efd
baraahalomari <balomari995@gmail.com>
2021-09-05 13:05:05 +0300

    admin

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -1,62 +1,66 @@
-import React, { useContext,useEffect ,useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../../../context/auth'
 import axios from "axios";
 
 function Admin() {
-    const { token, user, setUser } = useContext(AuthContext);
-    const[users,UpdateAllusers]=useState([])
-    const[fetchafterDelete,UpdatefetchafterDelete]=useState([false])
+  const { token, user, setUser } = useContext(AuthContext);
+  const [users, UpdateAllusers] = useState([])
+  const [fetchafterDelete, UpdatefetchafterDelete] = useState([false])
+  useEffect(() => {
+    async function fetchData() {
+      console.log("tokken", token)
 
-    useEffect( ()  => {
-        async function fetchData() {
-            
 
-         
 
-        // will be filtered in order not to return (myself) to the lisr  {current user} as well
-            const allusers=await axios.get("http://localhost:3001/listusers", {
-                headers: {
-                  Authorization:token
-                }})
-                UpdateAllusers(allusers.data)
-                console.log(allusers.data)
-          }
-          fetchData();
-        }, [fetchafterDelete])
+      // will be filtered in order not to return (myself) to the lisr  {current user} as well
+      const allusers = await axios.get("https://spice-g4.herokuapp.com/listusers", {
+        headers: {
+          Authorization: token
+        }
+      })
+      UpdateAllusers(allusers.data)
+      console.log(allusers.data)
+    }
+    fetchData();
+  }, [fetchafterDelete])
 
-        async function remove (id)
-        {
-            console.log(id)
-            try {
-               const res=await axios.delete("http://localhost:3001/deleteUser/" + id, { userId: user._id }, {
-                  headers: {
-                    Authorization: 'Bearer ' + token 
-                  }
-                })
-                UpdatefetchafterDelete(!fetchafterDelete)
-          console.log(user)
-              } catch (err) { 
-        
-                console.log(err)
-              }}
-        
-    return (
-        <div>
-        {users?.map((item)=>{
-             return(
-                 <>
-                              <p>{item.email}</p>
-                         <p  onClick={()=>remove(item._id)} >Remove </p>
+  async function remove(id) {
+    console.log(user)
 
-                          
-                             
-                             
+    console.log(id)
+    try {
+      const res = await axios.delete("https://spice-g4.herokuapp.com/deleteUser/" + id, {
+        headers: {
+          Authorization: token
+        },
+        data: {
+          userId: user._id
+        }
+      })
+      UpdatefetchafterDelete(!fetchafterDelete)
+      console.log(user)
+    } catch (err) {
 
-                 </>
-             )
-         })}
-        </div>
-    )
+      console.log(err)
+    }
+  }
+
+  return (
+    <div>
+      {users?.map((item) => {
+        return (
+          <>
+           {(item.role !== 'admin') &&
+          <>
+            <p>{item.email}</p>
+            <p onClick={() => remove(item._id)} >Remove </p>
+            </>
+            }
+          </>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Admin
